76dbf624555ab715fb0df7eb432357f5bd7dea96
Bogdan12-dev <bogdankovalsky1@gmail.com>
2019-11-10 14:02:27 +0200

    JS

---
+++ CW/day1/index.js
@@ -0,0 +1,93 @@
+// "use strict";
+// console.log("Hello world");
+
+// let number = 6;
+// let string = "String";
+// let boolen = true; //false
+
+// let a = 2;
+// let b = 3;
+
+// let result = a + b;
+
+// console.log(result);
+
+// let apple;
+// console.log(apple);
+
+// console.log(a);
+// let a = 3;
+// var a = true;
+// console.log(typeof a);
+// var c = confirm("Дай денег");
+// console.log(c);
+// const z = prompt("Enter your card number");
+// console.log(z);
+// var d = promt("Enter pin code");
+// console.log(z);
+// let a = 9;
+
+// let b = 3;
+
+// let result = a % b;
+
+// console.log(result);
+
+// console.log(a-- - Number(b));
+// let a = "25px";
+
+// console.log(Number.parseInt(a) + 50 + "px");
+
+// const a = "asdf";
+
+// console.log(Number.IsNaN(+a));
+
+// console.log(0.1 + 0.3 === 0.4);
+
+// console.log((0.1 + 0.2).toFixed(1));
+
+// const a = 1,
+//   b = 5;
+// c = 8;
+// console.log(Math.max(a, b, c));
+
+// console.log(Math.floor(1.5));
+// console.log(Math.ceil(1.4));
+// console.log(Math.pow(3, 2));
+// console.log(Math.round(1.6));
+
+// const age = 25;
+
+// console.log("Hello I'm " + age + " years old");
+
+// const name = "Rodion";
+// console.log(name.lenght);
+// console.log(name.indexOf("o"));
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.includes());
+
+// const firstName = "Rodion";
+// const surName = "Romanov";
+// const age = 25;
+
+// console.log(
+//   `Hello my name is ${firstName} ${surName} and I'm ${age} years old`
+// );
+
+// const name = "Генератор защитного поля";
+// let price = 1000;
+// price = 2000;
+
+// console.log(`Выбран $(name) цена за штуку 2000 кредитов`);
+
+const day = 17;
+const month = 10;
+const year = 2048;
+
+console.log(date); // 17\10\2048
+console.log(message); // "Доброе утро, cегодня 17\10\2048, за бортом отличная погода!"
+const date = day + "/" + month + "/" + year;
+const message = `Доброе утро, cегодня $(date), за бортом отличная погода!`;
+console.log(date);
+console.log(message);
