f6ab9e8772f195af37859ede622db476b9bc62cb
Bogdan12-dev <55618988+Bogdan12-dev@users.noreply.github.com>
2020-04-18 15:29:44 +0300

    Add files via upload

---
+++ dop/index.js
@@ -0,0 +1,42 @@
+"use strict"
+// import * as deb
+const input = document.querySelector("#country");
+const listOfCountry = document.querySelector(".ListOfCountry");
+const countryInfo = document.querySelector(".countryInfo")
+const btn = document.querySelector(".push")
+function fetchCountries(searchQuery){
+    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}
+`)
+.then((res) => res.json())
+.then((data) => data)
+.catch((error) => erorr)
+}
+
+function inputHandler() {
+    fetchCountries(input.value).then((data) => {
+      console.log(data);
+      if (data.length > 2 && data.length < 10) {
+        const names = data.reduce(
+          (acc, el) => (acc += `<li>${el.name}</li>`),
+          ""
+        );
+        listOfCountry.innerHTML = names;
+      } else if (data.length === 1) {
+        const languages = data[0].languages.map(el => el.name).reduce((acc,el)=>(acc += `<li>${el}</li>`),"")
+        const countryInf = `<h2>${data[0].name}</h2>
+        <div style="display:flex">
+          <div>
+              <p>Capital: ${data[0].capital}</p>
+              <p>Population: ${data[0].population}</p>
+              <ul>${languages}</ul>
+          </div>
+          <div>
+              <img height="200" width="200" src="${data[0].flag}"/>
+          </div>
+        </div>`;
+        countryInfo.innerHTML = countryInf;
+      }
+    });
+  }
+  
+  btn.addEventListener("click", inputHandler);
